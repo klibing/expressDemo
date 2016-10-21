bab84daa0eb4c06284531346b053980ac42872f9
libing <klibing1234@126.com>
2016-10-21 12:40:56 +0800

    添加数据查询逻辑

--- db/testMysql.js
+++ db/testMysql.js
@@ -4,13 +4,13 @@
 
 var mysql = require("./db.js");
 
-mysql.query("select * from mytable", function (results) {
-   if(results && results.length > 0) {
-       for(var i=0;i<results.length;i++) {
-           var result = results[i];
-           console.log("first name:" + result["firstname"]);
-           console.log("last name:" + result["lastname"]);
-           console.log("message:" + result["message"]);
-       }
-   }
+mysql.query("insert into mytable(firstname, lastname, message) values('王', '月', '111个人信息'),('张', '三', 'as大是大非的流口水的福克斯')", function (results) {
+    if(results && results.length > 0) {
+        for(var i=0;i<results.length;i++) {
+            var result = results[i];
+            console.log("first name:" + result["firstname"]);
+            console.log("last name:" + result["lastname"]);
+            console.log("message:" + result["message"]);
+        }
+    }
 });
---
+++ routes/mytable.js
@@ -0,0 +1,27 @@
+/**
+ * Created by libing on 2016/10/21.
+ */
+var express = require("express");
+var mysql = require("../db/db.js");
+var router = express.Router();
+
+router.get("/list", function(request, response, next) {
+    //查询
+
+    mysql.query("select * from mytable", function (results) {
+        if(results && results.length > 0) {
+            for(var i=0;i<results.length;i++) {
+                var result = results[i];
+                console.log("first name:" + result["firstname"]);
+                console.log("last name:" + result["lastname"]);
+                console.log("message:" + result["message"]);
+            }
+        }
+
+        //response.render("mytable", {"rows" : JSON.stringify(results)});
+        response.render("mytable", {"rows" : results});
+
+    });
+});
+
+module.exports = router;
